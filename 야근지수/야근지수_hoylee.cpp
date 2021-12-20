76f466fa2a2e9481e940d20a79af43c262c5e330
hey-chocopie <ghdyd6372@naver.com>
2021-12-20 17:52:55 +0900

    야근지수 | update

---
+++ 야근지수/야근지수_hoylee.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <queue> 
+using namespace std;
+
+void wekly_work(int &n, priority_queue<long long> &pq)
+{
+    while(n != 0 && !(pq.empty()))
+    {
+        int max_num = pq.top();
+        
+        pq.pop();
+        if(max_num != 0)
+            pq.push(--max_num);
+        n--;
+    }
+    return ;
+}
+
+void Night_Shift(priority_queue<long long> &pq, long long &answer)
+{
+    while(!pq.empty())
+    {
+        answer += pq.top() * pq.top();
+        pq.pop();
+    }
+}
+
+long long solution(int n, vector<int> works) {
+    long long answer = 0;
+    priority_queue<long long> pq;
+    
+    for(int i = 0; i < works.size(); i++)
+        pq.push(works[i]);
+    wekly_work(n, pq);
+    Night_Shift(pq, answer);
+
+    return answer;
+}
